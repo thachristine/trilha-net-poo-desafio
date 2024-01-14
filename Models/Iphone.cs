f730f49847d84335dca7da5722c78106f47c67bd
Thais Christine <156492755+thachristine@users.noreply.github.com>
2024-01-14 17:35:40 -0300

    Update Iphone.cs
    tive que colocar manualmente pois a versão do meu computador nao permite transferir do vscode para Github.
    
    desafio concluido

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
+    // Concluido!!!
     public class Iphone
     {
+        public Iphone(string numero, string modelo, string ime, int memoria) :
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        base(numero,modelo,ime,memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+
+        {
+            Console.WriteLine($"Instalando o {nomeApp} - Acessar a Apple Store para baixar mais aplicativos no seu Iphone");
+        }
     }
 }
