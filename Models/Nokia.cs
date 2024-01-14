7135d75a83c4644482f9e210d4f3e6a5ec310461
Thais Christine <156492755+thachristine@users.noreply.github.com>
2024-01-14 17:37:35 -0300

    Update Nokia.cs
    tive que colocar manualmente, pois meu computador não permitiu transferir do vscode para Github por causa da verão
    
    desafio concluído

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -3,6 +3,19 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Nokia
     {
+        public Nokia(string numero, string modelo, string imei, int memoria):
+        base(numero,modelo,imei,memoria)
+        {
+
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+
+        {
+            Console.WriteLine($"Instalando o {nomeApp} - Acessar o Play Store para baixar mais aplicativos no seu Nokia");
+        }
+    
     }
+
+
 }
